0a8289a3ebd7f3083c12c5e435e9ce5abaaf900e
Ivan Kostrulev <imkostrulev@avito.ru>
2019-03-07 09:45:36 +0300

    complete first lab

--- Lab_1/MUD.py
+++ Lab_1/MUD.py
@@ -22,9 +22,10 @@ def start():
         way = ''
     current_location = 'square'
     locations = {'square', 'forge', 'mill', 'inn', 'road'}
-    while way != 'exit':
+    while way != 'quit':
         print('Choose: {}/{}/{}/{}/{}'.format(*locations))
-        print('\nType exit to finish game')
+        print('Type search to find something here')
+        print('\nType "quit" to finish game')
         way = input()
         if current_location != way:
             if way == 'road':
@@ -53,6 +54,7 @@ def square():
         '*____________________________*\n'
         '|   You are on the square    |\n'
         '*____________________________*\n'
+        '\n A small village square with diverging streets in different directions and framed by low wooden houses'
     )
 
 
@@ -62,6 +64,7 @@ def inn():
         '*____________________________*\n'
         '| You are in front of the inn|\n'
         '*____________________________*\n'
+        '\n You are in front of the entrance to a small but cozy inn.'
     )
 
 
@@ -71,6 +74,7 @@ def road():
         '*____________________________*\n'
         '|    You are on the road     |\n'
         '*____________________________*\n'
+        '\n You see a long road going to the horizon, a clear field and nothing else'
     )
 
 
@@ -80,6 +84,7 @@ def mill():
         '*____________________________*\n'
         '|      You are nearby mill   |\n'
         '*____________________________*\n'
+        '\n'
     )
 
 
@@ -89,8 +94,24 @@ def forge():
         '*____________________________*\n'
         '|    You are nearby forge    |\n'
         '*____________________________*\n'
+        '\n'
     )
 
+
+def search(current_location):
+    print('You are searching something here...')
+    if current_location == 'square':
+        print('You found a bunch of garbage here and nothing more.')
+    elif current_location == 'inn':
+        print('There is nothing useful here.')
+    elif current_location == 'road':
+        print('Only one broken wheel in the bushes.')
+    elif current_location == 'mill':
+        print('A few bags on the cart at the mill and thick and prickly bushes around. You do not want to go there.')
+    elif current_location == 'forge':
+        print('Here is a pile of scrap metal, but you don’t see anything useful for yourself.')
+    else:
+        print('Where are you man?? And how did you come there?')
 
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
